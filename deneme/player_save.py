72254bb6c7bc8357967213146b7d78f6ef4c6709
Cagri Esen <esencgr@gmail.com>
2020-10-11 19:57:40 +0300

    deneme

---
+++ deneme/player_save.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+print( "Player Save Program")
+
+name = input( "Name : " )
+surname = input( "Surname : " )
+team = input( "Team : ")
+
+list_info = [ name, surname, team ]
+print( f"Player name : { list_info[0] }\nSurname : { list_info[1] }\nTeam : { list_info[2] } " )
