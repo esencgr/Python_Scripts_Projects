83fff25becf8907aa84d10d12820e3e97472a9bd
Cagri Esen <esencgr@gmail.com>
2021-05-22 13:02:16 +0300

    add

--- Task/image_to_text.py
+++ Task/image_to_text.py
@@ -9,20 +9,10 @@ def img_to_str(img):
 if __name__ == "__main__":
     os.environ['DISPLAY'] = ':0'   
     # creating a object 
-    img = Image.open("/home/cgr/Python/Task/images/ocr.png") 
+    img = Image.open("/home/cgr/Python/Python_Scripts_Projects/Task/images/ocr.png") 
     img.show()
-
 
     # text_data = pytesseract.image_to_string(img.convert('RGB'), lang='eng')
     text_data = img_to_str(img)
     print(text_data)
-
-    # m = re.search("TOTAL (\w+)", text_data)
-    # total = m[1]
-    # print(total)
-
-    m = re.search("Balance Due (\S+)", text_data)
-    balance = m[1]
-    print(f"balance due : {balance}")
-
 
